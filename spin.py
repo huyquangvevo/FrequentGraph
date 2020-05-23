ac61b8279220fa8c0f8e0eaaefbde25a275c6ac8
QuangHuy <quanghuy27101997@gmail.com>
2020-05-23 17:53:39 +0700

    building frequent tree

--- spin.py
+++ spin.py
@@ -1,10 +1,15 @@
 from utils import readGraphs
-from algorithms import getFrequentEdges,frequentGraph
+from algorithms import GraphCollections
 
 
 graphs = readGraphs('mico.outx')
 
 
 if __name__ == "__main__":
-    frequents = getFrequentEdges(graphs,0.8)
-    frequentGraph(graphs,frequents)
+    # frequents = getFrequentEdges(graphs,0.8)
+    # print(frequents)
+    # frequentGraph(graphs,frequents)
+    graphDB = GraphCollections(graphs,0.8)
+    freqGraphs = graphDB.frequentGraph()
+    # print(freqGraphs)
+    
