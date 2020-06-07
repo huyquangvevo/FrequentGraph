4815c9797be8e4f5bdbc9009482f0ca6e2212a37
QuangHuy <quanghuy27101997@gmail.com>
2020-06-07 10:20:06 +0700

    change datasets

--- spin.py
+++ spin.py
@@ -5,7 +5,7 @@ from algorithm import string2matrix
 import numpy as np
 import json 
 
-datasets = "mico-demo"
+datasets = "mico"
 graphs = readGraphs('{}.outx'.format(datasets))
 
 def extractResultGraph(results):
